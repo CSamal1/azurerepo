e6157b30f5b02946dd6edf7213719ede98763b04
CSamal1 <164157514+CSamal1@users.noreply.github.com>
2024-03-20 17:07:30 +0530

    Add files via upload

---
+++ table.sql
@@ -0,0 +1,19 @@
+create table Employee6
+(
+empid varchar(15),
+empname varchar(10),
+department varchar(15),
+salary int
+)
+
+insert into employee
+values('chi47', 'chiranjeeb', 'it', 20000)
+
+insert into employee
+values('chi43', 'chira', 'it', 20000)
+
+insert into employee
+values('chi49', 'chi', 'it', 20000)
+
+insert into employee
+values('chi4', 'samal', 'it', 20000)
